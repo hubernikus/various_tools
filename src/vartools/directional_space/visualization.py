abb8d026c751cddeeeecef52865dbc8bca3a2513
Lukas Huber <hubernikus@gmail.com>
2021-08-03 18:57:58 +0200

    update vizualization file to allow circular patch with color

--- src/vartools/directional_space/visualization.py
+++ src/vartools/directional_space/visualization.py
@@ -28,7 +28,11 @@ def pathify(polygon):
         + [ring_coding(r) for r in polygon.interiors])
     return Path(vertices, codes)
 
-def circular_space_setup(ax, circ_radius=pi/2):
+def circular_space_setup(ax, circ_radius: float = pi/2, space_radius: float = pi,
+                         circle_background_color='white', outer_boundary_color=None):
+    """ Draws circle space on axis with with (dotted) circle-radius 'circ-radius' and
+    outer space-boundaries of radius 'space_radius'.
+    """
     ax.spines.left.set_position('center')
     ax.spines.right.set_color('none') 
     ax.spines.bottom.set_position('center')
@@ -36,18 +40,30 @@ def circular_space_setup(ax, circ_radius=pi/2):
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
 
-    ax.axis('equal')
-    circ_var = np.linspace(0, 2*pi, 100)
-    rad = pi
-    ax.plot(np.cos(circ_var)*circ_radius, np.sin(circ_var)*circ_radius, 'k--')
+    # circ_var = np.linspace(0, 2*pi, 100)
+    # rad = pi
+    # ax.plot(np.cos(circ_var)*circ_radius, np.sin(circ_var)*circ_radius, 'k--')
+
+    if outer_boundary_color is not None:
+        polygon = Point(0, 0).buffer(space_radius).difference(Point(0,0).buffer(circ_radius))
+        path = pathify(polygon)
+        patch = PathPatch(path, facecolor=outer_boundary_color, linewidth=0)
+        ax.add_patch(patch)
+        
+    polygon = Point(0, 0).buffer(circ_radius)
+    path = pathify(polygon)
+    patch = PathPatch(path, facecolor=circle_background_color, linewidth=1, linestyle='--')
+    ax.add_patch(patch)
 
     # polygon = Point(0, 0).buffer(10.0).difference(MultiPoint([(-5, 0), (5, 0)]).buffer(3.0))
-    polygon = Point(0, 0).buffer(10.0).difference(Point(0,0).buffer(pi))
+    polygon = Point(0, 0).buffer(10.0).difference(Point(0,0).buffer(space_radius))
     path = pathify(polygon)
     patch = PathPatch(path, facecolor='white', linewidth=0)
     ax.add_patch(patch)
 
+    ax.axis('equal')
+    
     dx = dy = 0.1
-    outer_circle_limits = pi
+    outer_circle_limits = space_radius
     ax.set_xlim([-outer_circle_limits-dx, outer_circle_limits+dx])
     ax.set_ylim([-outer_circle_limits-dy, outer_circle_limits+dy])
