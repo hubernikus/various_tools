a438df745165f37abb12ed97ec1e4176d090455f
Lukas Huber <hubernikus@gmail.com>
2021-08-05 23:35:24 +0200

    rename functions

--- scripts/visualize_dynamical_system.py
+++ scripts/visualize_dynamical_system.py
@@ -9,7 +9,7 @@ from vartools.dynamical_systems import PendulumDynamics, DuffingOscillator, Bifu
 from vartools.dynamical_systems import plot_dynamical_system_streamplot
 
 
-def visualize_pendulum():
+def visualize_pendulum_system():
     my_dynamics = PendulumDynamics(maximum_velocity=1)
     plot_dynamical_system_streamplot(
         DynamicalSystem=my_dynamics,
@@ -18,7 +18,7 @@ def visualize_pendulum():
     plt.title("Pendulum Dynamics")
 
 
-def visualize_duffing():
+def visualize_duffing_system():
     my_dynamics = DuffingOscillator(maximum_velocity=1)
     plot_dynamical_system_streamplot(DynamicalSystem=my_dynamics,
                                      # x_lim=[-3.5, 3.5], y_lim=[-6, 6], axes_equal=False)
@@ -26,7 +26,7 @@ def visualize_duffing():
     plt.title("Duffing Oscillator")
 
 
-def visualize_spiral():
+def visualize_spiral_system():
     my_dynamics = BifurcationSpiral(maximum_velocity=1)
     plot_dynamical_system_streamplot(DynamicalSystem=my_dynamics,
                                      # x_lim=[-6.0, 6.0], y_lim=[-2.5, 6.5], axes_equal=False)
@@ -39,8 +39,8 @@ def visualize_sinuswave_system(save_figure=False):
     from vartools.dynamical_systems import SinusAttractorSystem
     
     trimmer = ConstVelocityDecreasingAtAttractor(const_velocity=1.0, distance_decrease=1.0)
-    my_dynamics = SinusAttractorSystem(
-        trimmer=trimmer, attractor_position=np.zeros(2))
+    my_dynamics = SinusAttractorSystem(trimmer=trimmer, attractor_position=np.zeros(2))
+        
 
     plot_dynamical_system_streamplot(
         DynamicalSystem=my_dynamics,
@@ -52,14 +52,12 @@ def visualize_sinuswave_system(save_figure=False):
         figure_name = "sinus_attractor_system"
         plt.savefig("figures/" + figure_name + ".png", bbox_inches='tight')
 
-    
 
 if (__name__) == '__main__':
     plt.ion()
-    # visualize_pendulum()
-    # visualize_duffing()
-    # visualize_spiral()
+    # visualize_pendulum_system()
+    # visualize_duffing_system()
+    # visualize_spiral_system()
     visualize_sinuswave_system(save_figure=True)
-
 
 print('Done')
