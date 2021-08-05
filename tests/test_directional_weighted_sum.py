041fd648206e917d0f73d8b2f5d442040c21eea1
Lukas Huber <hubernikus@gmail.com>
2021-08-05 23:34:40 +0200

    add new test

---
+++ tests/test_directional_weighted_sum.py
@@ -0,0 +1,99 @@
+#!/USSR/bin/python3.9
+""" Test the directional space. """
+# Author: LukasHuber
+# Date: 2021-05-18
+# Email: lukas.huber@epfl.ch
+# License: BSD (c) 2021
+
+import unittest
+import copy
+
+from math import pi
+
+import numpy as np
+from numpy import linalg as LA
+
+from vartools.linalg import get_orthogonal_basis
+
+from vartools.directional_space import get_angle_space
+from vartools.directional_space import get_angle_space_inverse
+from vartools.directional_space import get_angle_from_vector, get_vector_from_angle
+
+from vartools.directional_space import UnitDirection, DirectionBase
+from vartools.directional_space import get_directional_weighted_sum
+
+class TestDirecionalSum(unittest.TestCase):
+    def test_directional_weighting(self):
+        dim = 3
+        n_dir = 2
+        
+        null_direction = np.array([1, 0, 0])
+        
+        directions = np.zeros((dim, n_dir))
+        directions[:, 0] = [0.0, 1, 0]
+        directions[:, 1] = [0.0, -1, 0]
+        
+        weights = np.array([0.5, 0.5])
+
+        summed_dir = get_directional_weighted_sum(
+            null_direction=null_direction, directions=directions, weights=weights)
+
+        self.assertTrue(np.allclose(summed_dir, np.array([1, 0, 0])))
+
+    def test_decomposition_directional_weighted_sum(self):
+        dim = 3
+        n_dir = 2
+        
+        null_direction = np.array([-0.4, 0.4, 0.3])
+        # null_direction = null_direction / LA.norm(null_direction)
+        
+        directions = np.zeros((dim, n_dir))
+        directions[:, 0] = [0.1, 2, 0.2]
+        directions[:, 1] = [0.3, -1, 0]
+        # directions = directions / np.tile(LA.norm(directions, axis=0), (dim, 1))
+
+        weights = np.array([0.3, 0.7])
+
+        summed_dir = get_directional_weighted_sum(
+            null_direction=null_direction, directions=directions, weights=weights)
+
+        matr = np.hstack((null_direction.reshape(dim, 1), directions))
+
+        components = LA.pinv(matr) @ summed_dir
+        self.assertTrue(all(components >= 0))
+
+
+    def test_execution_multiple_higher_dimension(self):
+        """ Higher dimension test without true-statement. """
+        dim = 4
+        n_dir = 5
+        
+        null_direction = np.array([-0.4, 0.4, 0.3, 0.9])
+        # null_direction = null_direction / LA.norm(null_direction)
+        
+        directions = np.zeros((dim, n_dir))
+        directions[:, 0] = [0.1, 2, 0.2, 0.1]
+        directions[:, 1] = [0.3, -1, 0, -0.9]
+        directions[:, 2] = [0.2, 4, -1, 3]
+        directions[:, 3] = [1, 0, -2, 3.0]
+        directions[:, 4] = [4, -1, 2, 8.0]
+        # directions = directions / np.tile(LA.norm(directions, axis=0), (dim, 1))
+
+        weights = np.array([0.3, 0.7, 0.1, -0.1, 0.1])
+        summed_dir = get_directional_weighted_sum(
+            null_direction=null_direction, directions=directions, weights=weights)
+
+        matr = np.hstack((null_direction.reshape(dim, 1), directions))
+        
+        # Correct angle through previous implementation
+        correct_angle = np.array([-0.15150807, -0.77723109,  0.58805302,  0.16477494])
+
+        self.assertTrue(np.allclose(summed_dir, correct_angle))
+
+if (__name__) == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+    
+    user_test = False
+    if user_test:
+        Tester = TestDirecionalSum()
+        Tester.test_directional_weighting()
