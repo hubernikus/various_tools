042370074f474d599b6f9b328ce448d9bf9c77d6
Lukas Huber <hubernikus@gmail.com>
2021-10-27 02:02:43 +0200

    add files for clean repo

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from setuptools import setup
-
-setup(name='vartools',
-      version='0.1',
-      description='Various Tools',
-      author='Lukas Huber',
-      author_email='lukas.huber@epfl.ch',
-      packages=[
-          'vartools',
-                ],
-      scripts=[
-          'scripts/main.py',
-          ],
-      package_dir={'': 'src'}
-     )
+setup(name="my_pagacke_name", packages=find_packages())
