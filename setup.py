fc712a4f600a316dc3571e8eddd1aaa21ec10698
Lukas Huber <hubernikus@gmail.com>
2021-11-09 22:39:42 +0100

    fix setup py file

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(packages=find_packages())
+setup(
+    name="vartools",
+    version="0.1",
+    description="Various Tools",
+    author="Lukas Huber",
+    author_email="lukas.huber@epfl.ch",
+    # packages=find_packages(),
+    scripts=[
+        "scripts/main.py",
+    ],
+    package_dir={"": "src"},
+)
