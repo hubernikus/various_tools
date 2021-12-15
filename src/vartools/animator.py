c8ef397c71488ff79b0ea79090271d963303df79
Lukas Huber <hubernikus@gmail.com>
2021-12-15 01:20:29 +0100

    update vartools

--- src/vartools/animator.py
+++ src/vartools/animator.py
@@ -1,15 +1,16 @@
 """ Class which is used as basis for animations."""
 # Author: Lukas Huber
-# Date: 2021-11-25"
-# Email: lukas.huber@epfl.ch
+# Date: 2021-11-25
+# Email: hubernikus@gmail.com
 
 from abc import ABC, abstractmethod
 import datetime
 
 import numpy as np
 
-from matplotlib import animation
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 
 class Animator(ABC):
@@ -25,19 +26,31 @@ class Animator(ABC):
     filetype:
 
 
+    Methods (Virtual)
+    -----------------
+    _custom_init: Initialization of variables and plot. Make sure that all the variables which
+    are needed in the main class are passed as member-variables
+    update_step: Update the simulation, but also the
+    has_converged: Optional function to set a convergence check
+
+
     Methods
     -------
+    subplots: use this suplots to create figure & axes which are further used in the
+    update_step:
 
     """
 
     def __init__(
         self,
+        *args,
         it_max: int = 100,
         dt_simulation: float = 0.1,
         dt_sleep: float = 0.1,
         animation_name=None,
         file_type=".mp4",
-    ):
+        **kwargs,
+    ) -> None:
         self.it_max = it_max
 
         self.dt_simulation = dt_simulation
@@ -49,17 +62,28 @@ class Animator(ABC):
         # Simulation parameter
         self.animation_paused = False
 
+        # Additional arguments are passed to the custom-init
+        self._custom_init(*args, **kwargs)
+
     def on_click(self, event) -> None:
-        # TODO: do space and forward/backwards event
         """Click event."""
+        # TODO: do space and forward/backwards event
         if self.animation_paused:
             self.animation_paused = False
         else:
             self.animation_paused = True
 
-    def create_figure(self, *args, **kwargs) -> None:
-        self.fig = plt.figure(*args, **kwargs)
+    # def figure(self, *args, **kwargs) -> matplotlib.figure.Figure:
+    # """ Creates an animation figure which can be paused, replayed and saved. """
+    # self.fig = plt.figure(*args, **kwargs)
+    # cid = self.fig.canvas.mpl_connect("button_press_event", self.on_click)
+    # return self.fig
+
+    def subplots(self, *args, **kwargs) -> matplotlib.figure.Figure:
+        """Creates an animation figure which can be paused, replayed and saved."""
+        self.fig, self.ax = plt.subplots(*args, **kwargs)
         cid = self.fig.canvas.mpl_connect("button_press_event", self.on_click)
+        return self.fig, self.ax
 
     def run(self, save_animation: bool = False):
         if save_animation:
@@ -86,7 +110,7 @@ class Animator(ABC):
 
         else:
             ii = 0
-            while ii < it_max:
+            while ii < self.it_max:
                 if not plt.fignum_exists(self.fig.number):
                     print("Stopped animation on closing of the figure.")
                     break
@@ -95,10 +119,10 @@ class Animator(ABC):
                     plt.pause(dt_sleep)
                     continue
 
-                self.update_step(ii, animation_run=False)
+                self.update_step(ii)
 
                 # Check convergence
-                if self.has_converged():
+                if self.has_converged(ii):
                     print(f"All trajectories converged at it={ii}.")
                     break
 
@@ -107,9 +131,10 @@ class Animator(ABC):
 
                 ii += 1
 
-    def has_converged(self) -> bool:
-        """(Optional) convergence check which is called during each animation run."""
-        return False
+    @abstractmethod
+    def _custom_init(self, *args, **kwargs) -> None:
+        """Setup the environment including creating the axes."""
+        pass
 
     @abstractmethod
     def update_step(self, ii: int) -> None:
@@ -121,3 +146,8 @@ class Animator(ABC):
         3. plotting
         """
         pass
+
+    def has_converged(self, ii: int) -> bool:
+        """(Optional) convergence check which is called during each animation run.
+        Returns boolean to indicate if the system has converged (to stop the simulation)."""
+        return False
