b465553ca4b24a9f709199d70342fb37a759a4d5
Lukas <huberniku@gmail.com>
2022-04-01 16:59:59 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="vartools",
-    version="0.1",
-    description="Various Tools",
-    author="Lukas Huber",
-    author_email="lukas.huber@epfl.ch",
-    # packages=find_packages(),
-    # scripts=[],
-    package_dir={"": "src"},
-)
+if __name__ == "__main__":
+    setup()
