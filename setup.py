8224e89e67ee47bcecad9439224ee329e60be6b9
Lukas <huberniku@gmail.com>
2022-04-03 12:13:24 +0200

    simplify setup tools for working environment with docker

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
-from setuptools import setup, find_packages
+# import os
+# from glob import glob
 
-setup(name="vartools", packages=find_packages())
+from setuptools import setup
+
+package_name = 'vartools'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Lukas Huber',
+    maintainer_email='lukas.huber@epfl.ch',
+    description='Various Mathematical Tools',
+    license='TODO',
+    # package_dir={'': 'src'},
+    tests_require=['pytest'],
+    # entry_points={
+        # 'console_scripts': ['simulation_loader = pybullet_ros2.simulation_loader:main',
+                            # 'pybullet_ros2 = pybullet_ros2.pybullet_ros2:main']
+    # }
+)
