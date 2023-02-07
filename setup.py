a7d92966d3fafd31d54277d74d81531bb2fcc9c7
Lukas Huber <hubernikus@gmail.com>
2023-02-07 12:18:21 +0100

    update package files

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ package_name = "vartools"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="0.0.1",
     packages=[package_name],
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="Lukas Huber",
     maintainer_email="lukas.huber@epfl.ch",
     description="Various Mathematical Tools",
-    license="TODO",
+    license_file=LICENSE,
     # package_dir={'': 'src'},
     tests_require=["pytest"],
     # entry_points={
