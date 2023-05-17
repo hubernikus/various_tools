4cda4afd6edf06f1aaabee520c87d0ce6ac05109
Lukas Huber <hubernikus@gmail.com>
2023-05-17 19:54:25 +0200

    add color helpers

--- vartools/colors.py
+++ vartools/colors.py
@@ -1,7 +1,13 @@
-def hex_to_rgb(value):
+def hex_to_rgba(value):
     value = value.lstrip("#")
     lv = len(value)
     return tuple(int(value[i : i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+
+def hex_to_rgba_float(value):
+    value = value.lstrip("#")
+    lv = len(value)
+    return tuple(int(value[i : i + lv // 3], 16) / 256.0 for i in range(0, lv, lv // 3))
 
 
 def rgb_to_hex(rgb):
